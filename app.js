8b4908340497df49aaeacadf63a0a6453cc548b0
ucis-cmd <ugisj06@gmail.com>
2023-07-28 14:45:25 +0300

    Add comments to app.js

--- app.js
+++ app.js
@@ -3,27 +3,27 @@ const axios = require("axios");
 
 const tellJoke = async (options) => {
 
-  const delay = (ms) => {
+  const jokeDelay = (ms) => { //Delay between joke's setup and delivery
     return new Promise((resolve) => setTimeout(resolve, ms));
   };
 
-  if(!options.both && !options.single && !options.twopart){
+  if(!options.both && !options.single && !options.twopart){//No joke selected
     console.log("Please select joke type");
     return;
   }
 
-  let link = "https://v2.jokeapi.dev/joke/Any?safe-mode";                                
+  let link = "https://v2.jokeapi.dev/joke/Any?safe-mode";//Selects a joke from both types                                
   if (options.single) link += "&type=single";
   if (options.twopart) link += "&type=twopart";
 
   try {
-    const res = await axios.get(link);
+    const res = await axios.get(link); //Get JokeAPI data
     const joke = res.data;
-    if (joke.type === "single") {
+    if (joke.type === "single") { //Display joke, based on it's type
       console.log(joke.joke);
     } else {
       console.log(joke.setup);
-      await delay(5000);
+      await jokeDelay(5000);
       console.log(joke.delivery);
     }
   } catch (err) {
